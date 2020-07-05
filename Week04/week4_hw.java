adf6462a6bae1aaf56c1e44f16e1c2855fcaa460
songhaopeng <jsong2018@icloud.com>
2020-07-05 23:42:45 +0800

    week4

---
+++ Week04/week4_hw.java
@@ -0,0 +1,150 @@
+// leetcode 153
+
+class Solution {
+  public int findMin(int[] nums) {
+    // If the list has just one element then return that element.
+    if (nums.length == 1) {
+      return nums[0];
+    }
+
+    // initializing left and right pointers.
+    int left = 0, right = nums.length - 1;
+
+    // if the last element is greater than the first element then there is no rotation.
+    // e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
+    // Hence the smallest element is first element. A[0]
+    if (nums[right] > nums[0]) {
+      return nums[0];
+    }
+    
+    // Binary search way
+    while (right >= left) {
+      // Find the mid element
+      int mid = left + (right - left) / 2;
+
+      // if the mid element is greater than its next element then mid+1 element is the smallest
+      // This point would be the point of change. From higher to lower value.
+      if (nums[mid] > nums[mid + 1]) {
+        return nums[mid + 1];
+      }
+
+      // if the mid element is lesser than its previous element then mid element is the smallest
+      if (nums[mid - 1] > nums[mid]) {
+        return nums[mid];
+      }
+
+      // if the mid elements value is greater than the 0th element this means
+      // the least value is still somewhere to the right as we are still dealing with elements
+      // greater than nums[0]
+      if (nums[mid] > nums[0]) {
+        left = mid + 1;
+      } else {
+        // if nums[0] is greater than the mid value then this means the smallest value is somewhere to
+        // the left
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
+
+
+
+// leetcode 126
+class Solution {
+    private static final int INF = 1 << 20;
+    private Map<String, Integer> wordId; // 单词到id的映射
+    private ArrayList<String> idWord; // id到单词的映射
+    private ArrayList<Integer>[] edges; // 图的边
+
+    public Solution() {
+        wordId = new HashMap<>();
+        idWord = new ArrayList<>();
+    }
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        int id = 0;
+        // 将wordList所有单词加入wordId中 相同的只保留一个 // 并为每一个单词分配一个id
+        for (String word : wordList) {
+            if (!wordId.containsKey(word)) { 
+                wordId.put(word, id++);
+                idWord.add(word);
+            }
+        }
+        // 若endWord不在wordList中 则无解
+        if (!wordId.containsKey(endWord)) {
+            return new ArrayList<>();
+        }
+        // 把beginWord也加入wordId中
+        if (!wordId.containsKey(beginWord)) {
+            wordId.put(beginWord, id++);
+            idWord.add(beginWord);
+        }
+
+        // 初始化存边用的数组
+        edges = new ArrayList[idWord.size()];
+        for (int i = 0; i < idWord.size(); i++) {
+            edges[i] = new ArrayList<>();
+        }
+        // 添加边
+        for (int i = 0; i < idWord.size(); i++) {
+            for (int j = i + 1; j < idWord.size(); j++) {
+                // 若两者可以通过转换得到 则在它们间建一条无向边
+                if (transformCheck(idWord.get(i), idWord.get(j))) {
+                    edges[i].add(j);
+                    edges[j].add(i);
+                }
+            }
+        }
+
+        int dest = wordId.get(endWord); // 目的ID
+        List<List<String>> res = new ArrayList<>(); // 存答案
+        int[] cost = new int[id]; // 到每个点的代价
+        for (int i = 0; i < id; i++) {
+            cost[i] = INF; // 每个点的代价初始化为无穷大
+        }
+
+        // 将起点加入队列 并将其cost设为0
+        Queue<ArrayList<Integer>> q = new LinkedList<>();
+        ArrayList<Integer> tmpBegin = new ArrayList<>();
+        tmpBegin.add(wordId.get(beginWord));
+        q.add(tmpBegin);
+        cost[wordId.get(beginWord)] = 0;
+
+        // 开始广度优先搜索
+        while (!q.isEmpty()) {
+            ArrayList<Integer> now = q.poll();
+            int last = now.get(now.size() - 1); // 最近访问的点
+            if (last == dest) { // 若该点为终点则将其存入答案res中
+                ArrayList<String> tmp = new ArrayList<>();
+                for (int index : now) {
+                    tmp.add(idWord.get(index)); // 转换为对应的word
+                }
+                res.add(tmp);
+            } else { // 该点不为终点 继续搜索
+                for (int i = 0; i < edges[last].size(); i++) {
+                    int to = edges[last].get(i);
+                    // 此处<=目的在于把代价相同的不同路径全部保留下来
+                    if (cost[last] + 1 <= cost[to]) {
+                        cost[to] = cost[last] + 1;
+                        // 把to加入路径中
+                        ArrayList<Integer> tmp = new ArrayList<>(now); tmp.add(to);
+                        q.add(tmp); // 把这个路径加入队列
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    // 两个字符串是否可以通过改变一个字母后相等
+    boolean transformCheck(String str1, String str2) {
+        int differences = 0;
+        for (int i = 0; i < str1.length() && differences < 2; i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                ++differences;
+            }
+        }
+        return differences == 1;
+    } 
+}
